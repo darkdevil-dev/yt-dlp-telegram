197cf662c80ddbaa0c6c15e3a2ac9d81d9464595
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-07-25 09:04:23 +0530

    Update example.config.py

--- example.config.py
+++ example.config.py
@@ -1,3 +1,3 @@
-token = ""  # telegram token
-logs = 2390049  # logs channel id, if none set to None
+token = "7303947096:AAGanX1MaMuBG7UBQ-bl--gx73up6i5a1LI"  # telegram token
+logs = -1002166821912  # logs channel id, if none set to None
 max_filesize = 50000000  # bytes
